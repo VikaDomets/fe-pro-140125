6b3ae2eb736e3d3cbe954d3e8f1ec27f1dacc6cb
VikaDomets <dometswat@icloud.com>
2025-02-16 17:52:37 +0200

    done
    done

---
+++ 01/hm.js
@@ -0,0 +1,28 @@
+// Завдання 1
+// Оголосіть дві змінні: productCount (числового типу), productName (рядкового типу).
+// Присвойте їм початкові значення (наприклад, productCount = 10, productName = "Milk").
+// Змініть значення обох змінних (наприклад, productCount = 15, productName = "Juice").
+// Виведіть кінцеві значення на екран (в консоль чи документ).
+
+let productCount = 10;
+let productName = "Milk";
+productCount = 15;
+console.log(productCount);
+productName = "Juice";
+console.log(productName);
+
+// Завдання 2
+// Оголосіть дві змінні: userSalary (числового типу), userPosition (рядкового типу).
+// Присвойте їм стартові значення на ваш вибір (наприклад, userSalary = 1000, userPosition = "Junior Developer").
+// Змініть значення обох змінних (наприклад, збільшіть зарплату та змініть посаду на "Middle Developer").
+// Виведіть кінцеві значення змінних в консоль (чи будь-яким іншим способом).
+
+let userSalary = 1000;
+console.log(userSalary);
+let userPosition = "Junior Developer";
+console.log(userPosition);
+
+userSalary = 2000;
+console.log(userSalary);
+userPosition = "Middle Developer";
+console.log(userPosition);
